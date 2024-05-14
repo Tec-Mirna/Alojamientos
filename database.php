ca17c9bb6ec8b0f6129b47237f47faa82e657992
Mirna <mirnarecinos382@gmail.com>
2024-05-13 21:11:16 -0300

    Conexión a la base de datos

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername  = "127.0.0.1:3307";
+$username = "root";
+$password = "";
+$dbname = "alojamientos";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+    
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+
+
+
+
