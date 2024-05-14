9ad7e014546cdade7376a4fd1f44bb34e300137a
Mirna <mirnarecinos382@gmail.com>
2024-05-13 21:17:10 -0300

    Inicio de sesión

---
+++ login.php
@@ -0,0 +1,192 @@
+<?php
+session_start(); // Iniciar sesión
+
+require_once './database.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario = $_POST["usuario"];
+    $contrasenia = $_POST["contrasenia"];
+
+    // Realizar la consulta SQL para verificar las credenciales
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND contrasenia = '$contrasenia'";
+    $result = $conn->query($sql);
+
+    // Verificar si los datos son correctos
+    if ($result && $result->num_rows > 0) {
+        // Credenciales válidas, establecer variable de sesión y redirigir al usuario al home
+        $_SESSION['logged_in'] = true;
+
+        // obtener el nombre del usuario de la base de datos
+        $row = $result->fetch_assoc();
+        $_SESSION['nombre_completo'] = $row['nombre_completo'];
+    
+        // Si los datos son correctos dirigir al home
+        header("Location: home.php");
+        exit();
+    } else {
+        // En caso que los datos sean incorrectos
+        $message = "Usuario o contraseña incorrectos";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Form</title>
+
+    
+
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: steelblue;
+  color: #fff;
+  font-family: 'Muli', sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+}
+
+.container {
+  background-color: rgba(0, 0, 0, 0.4);
+  padding: 20px 40px;
+  border-radius: 5px;
+}
+
+.container h1 {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.container a {
+  text-decoration: none;
+  color: lightblue;
+}
+
+.btn {
+  cursor: pointer;
+  display: inline-block;
+  width: 100%;
+  background: lightblue;
+  padding: 15px;
+  font-family: inherit;
+  font-size: 16px;
+  border: 0;
+  border-radius: 5px;
+}
+
+.btn:focus {
+  outline: 0;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.text {
+  margin-top: 30px;
+}
+
+.form-control {
+  position: relative;
+  margin: 20px 0 40px;
+  width: 300px;
+}
+
+.form-control input {
+  background-color: transparent;
+  border: 0;
+  border-bottom: 2px #fff solid;
+  display: block;
+  width: 100%;
+  padding: 15px 0;
+  font-size: 18px;
+  color: #fff;
+  position: relative;
+  z-index: 100;
+}
+
+.form-control input:focus,
+.form-control input:valid {
+  outline: 0;
+  border-bottom-color: lightblue;
+}
+
+.form-control label {
+  position: absolute;
+  top: 15px;
+  left: 0;
+  pointer-events: none;
+}
+
+.form-control label span {
+  display: inline-block;
+  font-size: 18px;
+  min-width: 5px;
+  transition: 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+.form-control input:focus + label span,
+.form-control input:valid + label span {
+  color: lightblue;
+  transform: translateY(-30px);
+}
+
+    </style>
+  </head>
+  
+  <body>
+
+  <?php if(!empty($message)): ?>
+    <div class="alert alert-danger" role="alert"><?php echo $message; ?></div>
+<?php endif; ?>
+
+
+
+    <div class="container">
+      <h1>Please Login</h1>
+      <form action="login.php" method="POST">
+        <div class="form-control">
+          <input  name="usuario" type="text" required>
+          <label>User</label>
+         
+        </div>
+
+        <div class="form-control">
+          <input  name="contrasenia" type="password" required>
+          <label>Password</label>
+        </div>
+
+        <input type="submit" value="Login"/>
+
+        <p class="text">Don't have an account? <a href="register.php">Register</a> </p>
+      </form>
+    </div>
+    <script >
+
+    const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+})
+    </script>
+  </body>
+</html>
+
+
