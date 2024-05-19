d9b5088ad907a9804fe18df484fa0a49a34ab06d
Mirna <mirnarecinos382@gmail.com>
2024-05-19 18:23:40 -0300

    Actualización de diseño

--- home.php
+++ home.php
@@ -1,5 +1,7 @@
+  <?php
 
-
+session_start();
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,49 +12,41 @@
     <title>AlojamientosES</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
- <style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css"><!-- íconos de bootstrap -->
+
+    <style>
   .container{
     margin-top: 30px;
   }
   .nav-col{
     background: #654ea3;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #eaafc8, #654ea3);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #eaafc8, #654ea3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+   background: -webkit-linear-gradient(to right, #eaafc8, #654ea3);  /* Chrome 10-25, Safari 5.1-6 */
+   background: linear-gradient(to right, #eaafc8, #654ea3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+   margin-bottom: 30px;
 
   }
   .cards{
     border: white;
   }
-
+ 
  </style>
 </head>
 <body>
-   
+
 <!-- Navbar -->
-<?php
-session_start();  /* Inicia la sesión y permite acceder a la variable */
-
-
-// Verificar si 'reservations' está definida y es un array, si no inicializarla como un array vacío
-if (!isset($_SESSION['reservations']) || !is_array($_SESSION['reservations'])) {
-  $_SESSION['reservations'] = [];
-}
-?>
-
-
 <nav class="navbar nav-col">
   <div class="container-fluid">
-    <a class="navbar-brand" >
-      <img src="https://cdn-icons-png.flaticon.com/512/7602/7602952.png" alt="Logo" width="40" height="36" class="d-inline-block align-text-top">
+    <a class="navbar-brand" href="./home.php">
+      <img src="https://cdn-icons-png.flaticon.com/512/7602/7602952.png" alt="Logo"  width="40" height="36" class="d-inline-block align-text-top">
       AlojamientoES
     </a>
     <div class="ml-auto">
       <?php if(isset($_SESSION['nombre_completo'])): ?> <!-- Verifica si existe la variable  -->
-        <span class="navbar-text ">  <?php echo $_SESSION['nombre_completo']; ?><!-- Mostrar el nombre del usuario de base de datos -->
+        <span class="navbar-text "><i class="bi bi-person-fill"></i>  <?php echo $_SESSION['nombre_completo']; ?><!-- Mostrar el nombre del usuario de base de datos -->
         <a  href="reservations.php" class="btn position-relative" style="margin-right: 10px;" >Reservas
      
-            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                  <?php echo count($_SESSION['reservations']); ?> <!-- Contador -->
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" id="reservationCount">
+               0
             </span>
          </a>
       </span>
@@ -61,6 +55,29 @@ if (!isset($_SESSION['reservations']) || !is_array($_SESSION['reservations'])) {
     </div>
   </div>
 </nav>
+
+<h1 style="color: purple; text-align:center;">Alojamientos de El Salvador</h1>
+
+
+<?php
+
+// Verificar si 'reservations' está definida y es un array, si no inicializarla como un array vacío
+if (!isset($_SESSION['reservations']) || !is_array($_SESSION['reservations'])) {
+  $_SESSION['reservations'] = [];
+}
+?>
+
+
+
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    const reservationCount = document.getElementById('reservationCount');
+    let reservations = JSON.parse(localStorage.getItem('reservations')) || [];
+    reservationCount.textContent = reservations.length;
+});
+ </script>
+
 
 
 </body>
@@ -86,12 +103,13 @@ while ($row = $result->fetch_assoc()) {
   echo "<div class='col-md-8'>";
   echo "<div class='card-body' >";
   echo "<h3 class='card-title'>{$row['nombre']}</h3>";
-  echo "<p class='card-text'>{$row['ubicacion']}</p>";
-  
+  echo "<p class='card-text'><i class='bi bi-geo-alt-fill'></i>{$row['ubicacion']}</p>";
+  echo "<p class='card-text'>{$row['descripcion']}</p>";
   echo "<form method='POST' action='reservations.php'>";
   echo "<input type='hidden' name='nombre' value='{$row['nombre']}'>";
   echo "<input type='hidden' name='ubicacion' value='{$row['ubicacion']}'>";
   echo "<input type='hidden' name='imagen' value='{$row['imagen']}'>";
+  echo "<input type='hidden' name='descripcion' value='{$row['descripcion']}'>";
   echo "<button type='submit' class='btn btn-warning btn-sm' style='position: absolute; bottom: 10px;'>Seleccionar</button>";
   echo "</form>";
   echo "</div>";
